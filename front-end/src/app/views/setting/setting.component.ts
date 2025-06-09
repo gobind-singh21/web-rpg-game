6c4df821db8fec2d759b9cb99a8887283dfc913f
Saurabh Rangdale <saurabhpr@google.com>
2025-06-09 16:12:26 +0530

    HomePage Done

---
+++ front-end/src/app/views/setting/setting.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'setting',
+  templateUrl: './setting.component.html',
+  styleUrls: ['./setting.component.css'],
+  imports: [CommonModule]
+})
+export class setting implements OnInit {
+
+  menuOpen: boolean = false;
+  animationPlayed: boolean = false; // To control 'from-arrow' animation on initial load
+
+  isRotated: boolean = false;
+
+  @HostBinding('class.background--blur')
+  get applyBackgroundBlur() {
+    return this.menuOpen;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    if (!this.animationPlayed) {
+      this.animationPlayed = true;
+    }
+
+    this.isRotated = !this.isRotated;
+
+    
+  }
+}
