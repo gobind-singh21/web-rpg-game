fe7e0ae9f4c3f90bdb7cf3ae5edefc0ad3e47a9a
Saurabh Rangdale <saurabhpr@google.com>
2025-06-11 11:28:09 +0530

    added add button and Updated UI

--- front-end/src/app/views/team-making/team-making.component.ts
+++ front-end/src/app/views/team-making/team-making.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { Tiles } from "../../shared/tiles/tiles.component";
+import { CharactercardComponent } from "../../shared/charactercard/charactercard.component";
 
 
 
 @Component({
     selector: 'team-making',
-    imports:[Tiles],
+    imports:[Tiles, CharactercardComponent],
     templateUrl: './team-making.component.html',
     styleUrl: './team-making.component.css'
 })
