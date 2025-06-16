158980297466204761478ddead27415322468ef8
Saurabh Rangdale <saurabhpr@google.com>
2025-06-16 18:41:20 +0530

    how-to-play-complete

--- front-end/src/app/views/how-to-play/how-to-play/how-to-play.component.ts
+++ front-end/src/app/views/how-to-play/how-to-play/how-to-play.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule for standalone components
+import { CommonModule } from '@angular/common'; // Needed for things like ngFor, ngIf
+import { RouterModule } from '@angular/router'; // Needed for routerLink
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-how-to-play',
-  standalone: true, // Mark as standalone component
-  imports: [CommonModule], // Add CommonModule for common Angular directives
+  standalone: true, // Mark component as standalone
+  imports: [CommonModule, RouterModule, MatIcon], // Import CommonModule and RouterModule
   templateUrl: './how-to-play.component.html',
   styleUrl: './how-to-play.component.css'
 })
 export class HowToPlayComponent {
-  // No specific logic needed for a static "How to Play" page
+
 }
